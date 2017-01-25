b913ea39de1ef42e345e20a79b7f5a461bf063c0
Grant Merrill <grant.merrill@live.com>
2017-01-24 20:16:56 -0700

    Added a checker for git status

---
+++ windows/emacsStart.bat
@@ -0,0 +1,22 @@
+@echo off
+for /f %%i in ('git rev-parse @{u}') do set UPSTREAM=%%i
+for /f %%i in ('git rev-parse @') do set LOCAL=%%i
+for /f %%i in ('git rev-parse %UPSTREAM%') do set REMOTE=%%i
+for /f %%i in ('git merge-base @ %UPSTREAM%') do set BASE=%%i
+
+if %LOCAL%==%REMOTE% (
+    echo "Up-to-date"
+    goto EOF
+    )
+if %LOCAL%==%BASE% (
+    echo "Need to pull"
+    goto EOF
+    )
+if %REMOTE%==%BASE% (
+    echo "Need to push"
+    goto EOF
+    )
+
+echo "Diverged"
+
+:EOF
